59ee6d82436c45bea750b60fec0417783773c561
Alan Pita <pitaman512@gmail.com>
2021-04-09 16:36:29 -0500

    fixed: setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "elevated_objects"},
-    packages=setuptools.find_packages(where="elevated_objects"),
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
     python_requires=">=3.6",
 )
